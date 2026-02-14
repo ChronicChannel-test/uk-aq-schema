62728d75aaef32c98c4a0e71f4babba74548512b
cic-test1 <cic-test1@sleepercar.co.uk>
2026-02-14 00:30:39 +0000

    Update uk_aq_security.sql

--- schemas/main_db/uk_aq_security.sql
+++ schemas/main_db/uk_aq_security.sql
@@ -94,6 +94,10 @@ alter default privileges in schema uk_aq_pop
 alter default privileges in schema uk_aq_pop
   grant execute on functions to service_role;
 
+-- PostGIS types/functions live in public on Supabase; service_role needs schema
+-- usage to write/read geography columns via PostgREST.
+grant usage on schema public to service_role;
+
 -- View grants (core PM2.5 rollups).
 alter view if exists uk_aq_core.la_latest_pm25 set (security_invoker = true);
 alter view if exists uk_aq_core.pcon_latest_pm25 set (security_invoker = true);
